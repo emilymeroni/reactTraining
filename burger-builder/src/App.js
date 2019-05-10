5529dfb5b6a3754677bd043a82d71f40f717da60
emilymeroni <emily.1989@hotmail.it>
2019-05-10 16:59:42 +0200

    route protection

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -6,7 +6,13 @@ import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
 
-import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  withRouter,
+  Redirect
+} from 'react-router-dom'
 
 import * as actions from './store/actions'
 
@@ -18,21 +24,39 @@ class App extends Component {
   }
 
   render () {
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={Auth} />
+        <Route path='/' component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path='/logout' component={Logout} />
+          <Route path='/' component={BurgerBuilder} />
+          <Redirect to='/' />
+        </Switch>
+      )
+    }
+    
     return (
       <BrowserRouter>
         <div>
-          <Layout>
-            <Switch>
-              <Route path='/checkout' component={Checkout} />
-              <Route path='/orders' component={Orders} />
-              <Route path='/auth' component={Auth} />
-              <Route path='/logout' component={Logout} />
-              <Route path='/' component={BurgerBuilder} />
-            </Switch>
-          </Layout>
+          <Layout>{routes}</Layout>
         </div>
       </BrowserRouter>
     )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
@@ -44,7 +68,7 @@ const mapDispatchToProps = dispatch => {
 
 export default withRouter(
   connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(App)
 )
